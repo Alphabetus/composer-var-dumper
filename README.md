629384fb4fdbcebc161ce94f2b2408a14199b0e2
Alphabetus <d.gomes@whic.de>
2020-12-17 19:38:16 +0100

    Add 'keep log' option

--- README.md
+++ README.md
@@ -5,21 +5,27 @@ This package provides a simple var_dumper that allows you to transcript variable
 It generates the file `/var/alpha-dump/dump.txt` with the contents of given var.  
 
 ######  1. Basic Usage 
-1. >$ composer install alphabetus/var-dumper 
-1. ```
-   <?php 
-   
-   namespace App\Module;
-   
-   use AlphaLogger\Logger as Logger;
-   
-   class YourClass
-   {
-        public function foo()
-        {
-            $var = ...
-            Logger::log($var);
-        }
-   }
-   ```
+1. >$ composer install alphabetus/var-dumper
+1. The static method `::log()` can be used with 1 or 2 parameters:
+    - `::log($var)` - Logs only the **last call of the dump** and deletes older entries.
+    - `::log($var, $keep_log_boolean)` - if true, cumulates dumper calls in the same file. 
+
+###### Follow the example below
+```
+<?php 
+
+namespace App\Module;
+
+use AlphaLogger\Logger as Logger;
+
+class YourClass
+{
+    public function foo()
+    {
+        $var = ...
+        Logger::log($var, [$cumulate_log]);
+    }
+}
+```
+  
 
--- src/AlphaLogger/Logger.php
+++ src/AlphaLogger/Logger.php
@@ -4,19 +4,69 @@ namespace AlphaLogger;
 
 class Logger
 {
-    public static function log($content)
+    public static function log($content, $keep_log = false)
     {
-        // create /logs directory if not exists
+        $i = new Logger();
+        $i->createDirIfNotExists();
+        file_put_contents($i->getFile(), $i->generateOutput($content, $keep_log));
+    }
+
+    /**
+     * @return string
+     * Returns files path
+     */
+    protected function getFile()
+    {
+        return "../var/alpha-dump/dump.txt";
+    }
+
+    /**
+     * @return void
+     * If the logs folder is not created, creates it.
+     */
+    protected function createDirIfNotExists()
+    {
         if (!file_exists('../var/alpha-dump')) {
             mkdir('../var/alpha-dump', 0777, true);
         }
+    }
 
-        $file = "../var/alpha-dump/dump.txt"; // assigns files
-        ob_start(); // grabs dump in memory
-        var_dump($content); // dumps content
-        $output = ob_get_clean(); // assings memory dump to var
-        $output .= "\n"; // appends extra line to the file
-        $output .= file_get_contents($file);
-        file_put_contents($file, $output); // stores as append on file
+    /**
+     * @param $content
+     * @return false|string
+     * Generates and outputs the dump
+     */
+    protected function outputAndRecord($content)
+    {
+        ob_start();
+        var_dump($content);
+        $output = ob_get_clean();
+        return $output;
+    }
+
+    /**
+     * @param $content
+     * @param $keep_log
+     * @return string
+     * Generate the text output
+     * Enables / Disables look keeping
+     */
+    protected function generateOutput($content, $keep_log)
+    {
+        $o = $this->outputAndRecord($content);
+        $o .= "--------------------------------------\n";
+        if ($keep_log) { $o .= $this->getFileContents($this->getFile()); }
+        return $o;
+    }
+
+    /**
+     * @param $file
+     * @return false|string
+     * Reads file if exists, returns false if file is not there
+     */
+    protected function getFileContents($file)
+    {
+        if (file_exists($file)) { return file_get_contents($file); }
+        else {  return false; }
     }
 }
